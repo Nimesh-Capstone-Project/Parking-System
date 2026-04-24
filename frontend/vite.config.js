236d61f94f84fbc332a630ec6dd971b7baaa5a7b
Nimesh Sundaram Veeramani(UST <307365@ust.com>
2026-04-24 23:57:29 +0530

    Configmap-auth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+  resolve: {
+    dedupe: ["react", "react-dom"]
+  }
 });
-
